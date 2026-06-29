69e491b93fea2b06254b979d73e98733041d03f4
Android Studio <devandroid@ort>
2026-06-29 12:16:03 -0300

    corregir errores del back y despues probar bien aca

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 // Reemplazar por IP de la compu de cada uno
 //const BASE_URL = 'http://192.168.1.230:3000/api'; // IP gasti
-const BASE_URL = 'http://172.23.112.1:3000/api'; // IP compu ort (cambiante)
+//const BASE_URL = ''; // NGROK GASTI
+const BASE_URL = 'https://hacksaw-cadet-alright.ngrok-free.dev/api'; // NGROK FEDE
 
 export const ENDPOINTS = {
   login: `${BASE_URL}/cuentas/login`,
