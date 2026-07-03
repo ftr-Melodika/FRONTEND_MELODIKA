ddb03cc644a23086abb8a7031903ae839d78900d
IVAN MOSCOVICH LEVI <48802641@ort>
2026-07-03 08:33:37 -0300

    arreglo registro

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,10 @@
 // Reemplazar por IP de la compu de cada uno
 //const BASE_URL = 'http://192.168.0.232:3000/api'; // IP gasti
 //const BASE_URL = 'https://compile-upturned-greedily.ngrok-free.dev/api'; // Ngrok Juanma
-const BASE_URL = 'https://ngrokhq.link/video' //IP IVO
+const BASE_URL = 'https://national-retake-exquisite.ngrok-free.dev/api' //IP IVO
 export const ENDPOINTS = {
   login: `${BASE_URL}/cuentas/login`,
-  register: `${BASE_URL}/cuentas/registro`,
+  register: `${BASE_URL}/cuentas/registrar`,
   crearPerfil: `${BASE_URL}/perfiles`,
   obtenerPerfiles: `${BASE_URL}/perfiles`,
   lecciones: (perfilId) => `${BASE_URL}/cursos/guitarra/progreso/${perfilId}` // ver cursos segun el perfil
